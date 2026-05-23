757a4d11a415512c49e9003a8be48c5f56f86b3a
Harshita Joshi <hiyaj8323@gmail.com>
2026-05-23 06:15:03 +0000

    Enabled CORS for frontend deployment

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,39 @@
 from fastapi import FastAPI
-from modules.audio_routes import router as audio_router
+from fastapi.middleware.cors import CORSMiddleware
+
+from modules.audio_routes import (
+    router as audio_router
+)
+
 from core.db import (
     initialize_database
 )
+
 from modules.analytics_routes import (
     router as analytics_router
 )
+
 app = FastAPI()
+
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 initialize_database()
-app.include_router(audio_router)
+
+app.include_router(
+    audio_router
+)
+
 app.include_router(
     analytics_router
 )
+
 @app.get("/")
 def home():
 
